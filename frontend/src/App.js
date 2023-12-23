20786abf9a40e7dbea48dfc0b20d911eba8eea40
MichaelFromMars <fuchsm@gmx.ch>
2023-12-23 15:52:16 +0100

    control search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+import {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Seach from './components/Search';
 
 const App = () => {
+  const [word, setSearchWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
   return (
     <div>
       <Header title="Images Gallery" />
+      <Seach word={word} setWord={setSearchWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
